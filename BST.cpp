569ef35746d9743fdccdc19368d7c35f028dbda9
Chenyang <zhuch@seas.upenn.edu>
2013-11-29 19:39:03 -0500

    new data

--- BST.cpp
+++ BST.cpp
@@ -14,13 +14,13 @@ struct TreeNode{
 };
 
 class Solution{
-	public:
+	public:	
 	bool isValidBST(TreeNode *root){
 		if(!root)
 		{
 			return true;
 		}
-		return isValid(root,INT_MIN,INTMAX);
+		return isValid(root,1,10);
 	}
 	bool isValid(TreeNode *root,int min,int max)
 	{
@@ -41,5 +41,21 @@ class Solution{
 
 using namespace std;
 int main(int argc, char *argv[]) {
+	TreeNode a=TreeNode(5);
+	TreeNode b=TreeNode(3);
+	TreeNode c=TreeNode(7);
+	TreeNode d=TreeNode(2);
+	TreeNode e=TreeNode(4);
+	TreeNode f=TreeNode(6);
+	TreeNode g=TreeNode(8);
+	a.left=&b;
+	a.right=&c;
+	b.left=&d;
+	b.right=&e;
+	c.left=&f;
+	c.right=&g;
+	TreeNode *root=&a;
+	Solution sol;
+	cout<<sol.isValidBST(root)<<endl;
 	
 }
---
+++ plane.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+struct Point
+{
+	int x;
+	int y;
+	Point(int x,int y):x(x),y(y){};
+};
+
+class Solution{
+	public:
+	
+}
+
+
+int main(int argc, char *argv[]) {
+	Point a=Point(1,2);
+	
+}
